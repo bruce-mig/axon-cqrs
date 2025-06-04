2aa026cf6a4cf5f16d0755adafb4780a7526823a
bruce-mig <brucetmig@icloud.com>
2025-06-04 09:52:47 +0200

    rmv

--- command-service/src/main/java/com/github/bruce_mig/command_service/command/api/data/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.bruce_mig.command_service.command.api.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
--- command-service/src/main/java/com/github/bruce_mig/command_service/query/api/queries/GetProductsQuery.java
+++
@@ -1,4 +0,0 @@
-package com.github.bruce_mig.command_service.query.api.queries;
-
-public class GetProductsQuery {
-}
